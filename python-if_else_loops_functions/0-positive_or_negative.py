11a2237395f46a179a41e74338eb082e373e9fe0
gasstin <gastonl1994@gmail.com>
2022-09-05 11:13:18 -0700

    task 0

---
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# This program will assign a random signed number to the variable number
+# each time it is executed. Complete the source code in order to print whether
+# the number stored in the variable number is positive or negative.
+import random
+number = random.randint(-10, 10)
+if number > 0:
+    print(f"{number} is positive")
+elif number == 0:
+    print(f"{number} is zero")
+else:
+    print(f"{number} is negative")
