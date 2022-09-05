9d900cff62420ff2912d8976a08317a373a0a809
gasstin <gastonl1994@gmail.com>
2022-09-05 11:35:56 -0700

    task 1

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# This program will assign a random signed number to the variable number
+# each time it is executed. Complete the source code in order to print
+# the last digit of the number stored in the variable number.
+import random
+number = random.randint(-10000, 10000)
+str = f"Last digit of {number} is {number % 10}"
+if (number % 10) > 5:
+    print(f"{str} and is greater than 5")
+elif (number % 10) == 0:
+    print(f"{str} and is 0")
+elif (number % 10) <= 5:
+    print(f"{str} and is less than 6 and not 0")
