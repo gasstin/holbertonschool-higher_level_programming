1281d90a77a8d1ec41ca6cacf412c1ec544b0d2d
gasstin <gastonl1994@gmail.com>
2022-09-06 12:22:22 -0700

    task 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def arguments_count(arguments):
+    if (len(arguments) - 1) == 0:
+        print("0 arguments.")
+    elif (len(arguments) - 1) == 1:
+        print("1 argument:")
+        print("1 : {0}".format(arguments[1]))
+    else:
+        print("{0} arguments".format((len(arguments) - 1)))
+        for n in range(1, len(arguments)):
+            print("{0} : {1}".format(n, arguments[n]))
+
+
+if __name__ == "__main__":
+    import sys
+    arguments_count(sys.argv)
