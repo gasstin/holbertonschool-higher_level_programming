b908d9130d5e0c8fa97bb1e36c97a2d19f46fe45
gasstin <gastonl1994@gmail.com>
2022-09-06 07:35:48 -0700

    task 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -4,6 +4,6 @@
 # Numbers should be printed in ascending order, with two digits
 for n in range(0, 100):
     if n < 99:
-        print("{}".format( n ), end=', ')
+        print("{:d}".format(n), end=', ')
     else:
-        print("{}".format( n ))
+        print("{}".format(n))
