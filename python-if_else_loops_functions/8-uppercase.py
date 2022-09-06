2c60b10709acd7ed10d1e4c0efcc3744d48f91d7
gasstin <gastonl1994@gmail.com>
2022-09-06 08:25:08 -0700

    fixing task 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-# Write a function that prints a string
-# in uppercase followed by a new line.
+# Write a function that prints a string in uppercase followed by a new line.
 
 
 def uppercase(str):
