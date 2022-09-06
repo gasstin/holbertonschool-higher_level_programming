5594b1243f130162abf430e5a450f7f2ffa3e896
gasstin <gastonl1994@gmail.com>
2022-09-06 12:23:20 -0700

    task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,11 +4,11 @@ def arguments_count(arguments):
         print("0 arguments.")
     elif (len(arguments) - 1) == 1:
         print("1 argument:")
-        print("1 : {0}".format(arguments[1]))
+        print("1: {0}".format(arguments[1]))
     else:
         print("{0} arguments".format((len(arguments) - 1)))
         for n in range(1, len(arguments)):
-            print("{0} : {1}".format(n, arguments[n]))
+            print("{0}: {1}".format(n, arguments[n]))
 
 
 if __name__ == "__main__":
