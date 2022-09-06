373b6bcde0288e63e7048798883cf61781b6e80b
gasstin <gastonl1994@gmail.com>
2022-09-06 07:58:44 -0700

    task 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -2,9 +2,10 @@
 # Write a program that prints numbers from 0 to 99.
 # Numbers must be separated by ,, followed by a space
 # Numbers should be printed in ascending order, with two digits
+
+
 for n in range(0, 100):
     if n < 99:
-        m = "{:02}, "
-        print(m.format(n), end="")
+        print("{:02}, ".format(n), end="")
     else:
         print(n)
