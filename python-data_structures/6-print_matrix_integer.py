6caf3fd78a165e380481b7d38d3f540deec45e6a
gasstin <gastonl1994@gmail.com>
2022-09-12 11:53:32 -0700

    task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -7,8 +7,8 @@ def print_matrix_integer(matrix=[[]]):
         print('')
     else:
         for n in range(len(matrix)):
-            for m in range(len(matrix)):
-                if m < (len(matrix) - 1):
+            for m in range(len(matrix[n])):
+                if m < (len(matrix[n]) - 1):
                     print('{:d}'.format(matrix[n][m]), end=' ')
                 else:
                     print('{:d}'.format(matrix[n][m]))
