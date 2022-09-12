77a10d3c89bd7f205eb281c1e9d3c98ef29e3f46
gasstin <gastonl1994@gmail.com>
2022-09-12 12:45:12 -0700

    task 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -3,7 +3,7 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_add = ()
+    tup_add = ()
     tuple_a_aux = ()
     tuple_b_aux = ()
     if len(tuple_a) == 0:
@@ -20,6 +20,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     else:
         tuple_b_aux = tuple_b[0], tuple_b[1]
 
-    tuple_add = tuple_a_aux[0] + tuple_b_aux[0], tuple_a_aux[1] + tuple_b_aux[1]
-    return(tuple_add)
-
+    tup_add = tuple_a_aux[0] + tuple_b_aux[0], tuple_a_aux[1] + tuple_b_aux[1]
+    return(tup_add)
