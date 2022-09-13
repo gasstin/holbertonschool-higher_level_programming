0b2a06ce33060ce8c503dc79c6d968baf87d0171
gasstin <gastonl1994@gmail.com>
2022-09-13 07:29:13 -0700

    task 1

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Write a function that replaces all occurrences
+# of an element by another in a new list.
+
+
+def search_replace(my_list, search, replace):
+    if my_list:
+        aux_list = []
+        aux_list += list(map(lambda x: replace if x == search else x, my_list))
+        return (aux_list)
