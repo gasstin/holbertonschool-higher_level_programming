eb3cc20dd8125111f9126f1b8081534c55301c15
gasstin <gastonl1994@gmail.com>
2022-09-13 08:55:38 -0700

    task 3

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Write a function that returns a set of common elements in two sets.
+
+
+def common_elements(set_1, set_2):
+    if set_1 and set_2:
+        common = {x for x in set_1 if x in set_2}
+        return (common)
