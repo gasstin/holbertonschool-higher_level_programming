a889f4702ec137936632e1424987daaec80c960f
gasstin <gastonl1994@gmail.com>
2022-09-13 05:48:35 -0700

    task 11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -8,7 +8,7 @@ def delete_at(my_list=[], idx=0):
             return (my_list)
         else:
             if idx:
-                my_list[:] = my_list[0:idx] + my_list[-(idx - 2):]
+                my_list[:] = my_list[0:idx] + my_list[(idx + 1):len(my_list)]
             else:
                 my_list[:] = my_list[1:len(my_list)]
 
