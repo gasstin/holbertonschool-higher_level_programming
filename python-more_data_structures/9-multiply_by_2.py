f10b6de0e28602daf1bfbef3a332b4a76cb43b93
gasstin <gastonl1994@gmail.com>
2022-09-14 06:23:07 -0700

    task 9

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -3,9 +3,9 @@
 
 
 def multiply_by_2(a_dictionary):
+    new_dict = dict()
     if a_dictionary:
-        new_dict = dict()
         for k, v in a_dictionary.items():
             v = v * 2
             new_dict[k] = v
-        return (new_dict)
+    return (new_dict)
