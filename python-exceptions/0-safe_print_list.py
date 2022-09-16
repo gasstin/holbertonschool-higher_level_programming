722f35e040428d4c7c11f0e2a77b421486e9406d
gasstin <gastonl1994@gmail.com>
2022-09-16 06:30:48 -0700

    task 0

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# Write a function that prints x elements of a list.
+
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(x):
+            if my_list[i]:
+                print(f"{my_list[i]}", end='')
+                if (i == x-1):
+                    print()
+            else:
+                break
+        return (x)
+    except Exception:
+            print()
+            return (i)
