26be514efdd1473e82e2980d76c75c299caf6954
gasstin <gastonl1994@gmail.com>
2022-09-16 08:55:14 -0700

    task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-# Write a function that prints the first x elements of a list and only integers.
+# Write a function that prints the first x elements
+# of a list and only integers.
 
 
 def safe_print_list_integers(my_list=[], x=0):
@@ -7,12 +8,12 @@ def safe_print_list_integers(my_list=[], x=0):
         count = 0
         for n in range(x):
             if isinstance(my_list[n], int):
-                print("{:d}".format(my_list[n]), end ='')
+                print("{:d}".format(my_list[n]), end='')
                 count += 1
             else:
                 continue
         print()
     except IndexError:
         raise
-        
+
     return (count)
