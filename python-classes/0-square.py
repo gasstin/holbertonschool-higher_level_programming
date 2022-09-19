9ffeb388c0c6c5820a55f837890d675bbe3490bf
gasstin <gastonl1994@gmail.com>
2022-09-19 06:33:54 -0700

    tasl 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""Write an empty class Square that defines a square:
 
+"""
 
 class Square:
-    """Write an empty class Square that defines a square:
+    """This class creates a square
+
     """
     pass
