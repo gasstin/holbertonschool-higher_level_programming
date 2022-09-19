27675bc4d46746b01ded3cd82f66817857b18c3d
gasstin <gastonl1994@gmail.com>
2022-09-19 06:45:31 -0700

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Write an empty class Square that defines a square by:
+    based on 0.square.py
+"""
+
+
+class Square:
+    """This class creates a square
+
+    """
+    def __init__(self, size):
+        """
+        Args:
+            size (int): is the size of the square
+        """
+        self.__size = size
