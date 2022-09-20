d2575c9f55435db13418c91790d294dee7da28ed
gasstin <gastonl1994@gmail.com>
2022-09-20 16:40:21 -0700

    task 2

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -15,21 +15,26 @@ def matrix_divided(matrix, div):
         Return:
             the new matrix
     """
-    try:
-        if not div:
-             raise ZeroDivisionError('division by zero')
-        new_m = []
-        for i in range(len(matrix)):
-            new_m += [list(map(lambda x: round((x / div), 2), matrix[i]))]
-        return new_m
-    except TypeError:
-        if type(matrix[i]) is not int and type(matrix[i]) is not float:
-            return 'matrix must be a matrix (list of lists) of integers/floats'
-        if len(matrix[i]) != len(matrix[i]):
-            return 'Each row of the matrix must have the same size'
-        if type(div) is not int and type(div) is not float:
-            print('div must be a number')
-        return None
+
+    if not div:
+        raise ZeroDivisionError('division by zero')
+    if type(div) is not int and type(div) is not float:
+        raise TypeError('div must be a number')
+    for row in matrix:
+        if type(row) is not list:
+            raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
+        if len(row) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same \
+size")
+        for index in row:
+            if type(index) is not int and type(index) is not float:
+                raise TypeError('matrix must be a matrix (list of lists) of \
+integers/floats')
+    new_m = []
+    for i in range(len(matrix)):
+        new_m += [list(map(lambda x: round((x / div), 2), matrix[i]))]
+    return new_m
 
 if __name__ == "__main__":
     import doctest
