c2976276dc9198ca31eeb5cc339ddddcf0719f2c
gasstin <gastonl1994@gmail.com>
2022-09-20 07:54:59 -0700

    task 2

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+'''Write a function that divides all elements of a matrix.
+
+    matrix must be a list of lists of integers or floats,
+    otherwise raise a TypeError exception with the message
+    matrix must be a matrix (list of lists) of integers/floats
+
+    Returns a new matrix
+'''
+
+
+def matrix_divided(matrix, div):
+    """ a function that adds 2 integers
+
+        args:
+            matrix: is the matrix to divide
+            div: is the divisor number 
+        Return:
+            the new matrix
+    """
+    try:
+        new_m = []
+        for i in range(len(matrix)):
+            new_m += [list(map(lambda x: round((x / div), 2), matrix[i]))]
+        return new_m
+    except TypeError:
+        if type(matrix[i]) is not int and type(matrix[i]) is not float:
+            return 'matrix must be a matrix (list of lists) of integers/floats'
+        if len(matrix[i]) != len(matrix[i + 1]):
+            return 'Each row of the matrix must have the same size'
+        if type(div) is not int and type(div) is not float:
+            return 'div must be a number'
+    except ZeroDivisionError:
+        if not div:
+            return 'division by zero'
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/2-matrix_divided.txt")
