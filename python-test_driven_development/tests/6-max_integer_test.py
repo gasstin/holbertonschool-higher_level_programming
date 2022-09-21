46dea3e9e7eba5c864eba9e1a081f38de9124450
gasstin <gastonl1994@gmail.com>
2022-09-21 09:52:44 -0700

    task 6

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -8,8 +8,13 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_max(self):
         self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+        self.assertEqual(max_integer([1, 3, 2, 4]), 4)
+        self.assertEqual(max_integer([5, 3, 2, 4]), 5)
+        self.assertEqual(max_integer([5, 3, 2, -4]), 5)
+        self.assertEqual(max_integer([-3, -2, -1]), -1)
     
     def test_only(self):
         self.assertAlmostEqual(max_integer([1]), 1)
+        self.assertEqual(max_integer([]), None)
 #    def test_str(self):
 #        self.assertRaises(TypeError, max_integer([1, 3, 'a', 2]))
