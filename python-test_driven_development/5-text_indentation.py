f3a9c3c1f0e673c5461c400414b89d45420c3bbc
gasstin <gastonl1994@gmail.com>
2022-09-21 06:58:29 -0700

    task 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -2,9 +2,10 @@
 ''' Write a function that prints a text with 2 new lines after
     each of these characters: ., ? and :
 
-    ext must be a string, otherwise raise a TypeError exception with the message
-    text must be a string
-    There should be no space at the beginning or at the end of each printed line
+    ext must be a string, otherwise raise a TypeError exception with
+    the message text must be a string
+    There should be no space at the beginning or
+    at the end of each printed line
 '''
 
 
@@ -19,7 +20,7 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError('text must be a string')
     for i in range(len(text)):
-        if text[i - 1] in [".", "?", ":"]:
+        if text[i - 1] in [".", "?", ":"] and text[i] == ' ':
             continue
         else:
             print(f"{text[i]}", end='')
