482da4da430b43739db73261b13d8e6f9c6a955f
gasstin <gastonl1994@gmail.com>
2022-09-21 10:56:53 -0700

    task 1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+    Write a class Rectangle that defines a rectangle by:
+    (based on 0-rectangle.py)
+
+"""
+
+
+class Rectangle:
+    """
+        this class define a real rectangle
+
+    """
+    def __init__(self, width=0, height=0):
+        """
+            Args:
+                width (int): is the width of the rectangle
+                height (int): is the height of the rectangle
+
+        """
+        self.__height = height
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
