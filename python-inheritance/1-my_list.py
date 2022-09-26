f1125edb3f1cbc7559b05f34732311ff1fcf816e
gasstin <gastonl1994@gmail.com>
2022-09-26 06:48:57 -0700

    task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    Write a class MyList that inherits from list:
+"""
+
+
+class MyList(list):
+    """
+        MyList recives the attributes of list
+    """
+    def print_sorted(self):
+        """
+            that prints the list, but sorted (ascending sort)
+        """
+        print(sorted(self))
