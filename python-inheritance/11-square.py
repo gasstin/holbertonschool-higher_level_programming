c63eb917c337c006268a2f90c936bfe7767745c5
gasstin <gastonl1994@gmail.com>
2022-09-27 10:16:31 -0300

    adding task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    Write a class Square that inherits
+    from Rectangle (9-rectangle.py):
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle, BaseGeometry):
+    """
+        This class create a square
+
+        - width and height must be positive integers
+        validated by integer_validator
+        - the area() method must be implemented
+        - print() should print, and str() should return,
+        the square description
+    """
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
+
+    def __str__(self):
+        return f'[Square] {self.__size}/{self.__size}'
