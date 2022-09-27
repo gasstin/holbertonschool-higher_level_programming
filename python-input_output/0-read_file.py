e527de38738db44df6fe3ce1f7fb56bf2e5543e2
gasstin <gastonl1994@gmail.com>
2022-09-27 11:54:05 -0300

    fixing task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,4 +9,4 @@ def read_file(filename=""):
     Write a function that reads a text file (UTF8) and prints it to stdout:
     """
     with open(filename, encoding="utf-8") as my_file_open:
-        print(my_file_open.read())
+        print(my_file_open.read(), end='')
