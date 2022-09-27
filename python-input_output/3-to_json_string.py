c38f17bc5da142c44a255b9d28d2f4d914c23c2b
gasstin <gastonl1994@gmail.com>
2022-09-27 14:39:35 -0300

    adding task 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    Task 3
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+       Write a function that returns the JSON
+       representation of an object (string):
+    """
+    return json.dumps(my_obj)
