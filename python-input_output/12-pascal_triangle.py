4ee79e8a0b1865fb64e6d515fd377c001203ad2b
gasstin <gastonl1994@gmail.com>
2022-09-28 17:00:44 -0300

    adding task 12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -2,9 +2,6 @@
 """
     Task 12
 """
-import os
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
 def pascal_triangle(n):
