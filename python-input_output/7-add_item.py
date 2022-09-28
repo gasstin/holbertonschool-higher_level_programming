43b5b6b22ac98e5031ade1c22581b065aabc6a27
gasstin <gastonl1994@gmail.com>
2022-09-28 14:23:41 -0300

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
     Task 7: Write a script that adds all arguments to a Python list,
-    and then save them to a file:
+    and then save them to a file.
 """
 import os
 import sys
