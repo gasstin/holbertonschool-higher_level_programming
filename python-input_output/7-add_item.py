f684838283e268a5e1a36c0d181ab9ddc13d18c4
gasstin <gastonl1994@gmail.com>
2022-09-28 14:21:04 -0300

    fixing task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,9 +9,10 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 filename = "add_item.json"
-aux_list = []
 if os.path.exists(filename):
     aux_list = load_from_json_file(filename)
+else:
+    aux_list = []
 for arg in sys.argv[1:]:
     aux_list.append(arg)
 save_to_json_file(aux_list, filename)
