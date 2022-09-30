3422cfb2c8bd7795adf6a03c26a553ed64dd7999
gasstin <gastonl1994@gmail.com>
2022-09-30 11:31:35 -0300

    fixing task 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,59 +1,59 @@
 #!/usr/bin/python3
 """
-	Write the class Rectangle that inherits from Base
+    Write the class Rectangle that inherits from Base
 """
 from models.base import Base
 
 
 class Rectangle(Base):
-	"""
-		This class creates a rectangle.
-	"""
+    """
+        This class creates a rectangle.
+    """
 
-	def __init__(self, width, height, x=0, y=0, id=None):
-		"""
-			Args:
-				width (int): is the width of the rectangle
-				height (int): is the height of the rectangle
-				x: is the x position of the rectangle
-				y: is the y position of the rectangle
-				id(int): is the identification of rectangle
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+            Args:
+                width (int): is the width of the rectangle
+                height (int): is the height of the rectangle
+                x: is the x position of the rectangle
+                y: is the y position of the rectangle
+                id(int): is the identification of rectangle
 
-		"""
-		super().__init__(id)
-		self.width = width
-		self.height = height
-		self.x = x
-		self.y = y
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
-	@property
-	def width(self):
-		  return self.__width
+    @property
+    def width(self):
+        return self.__width
 
-	@width.setter
-	def width(self, value):
-		self.__width = value
+    @width.setter
+    def width(self, value):
+        self.__width = value
 
-	@property
-	def height(self):
-		return self.__height
+    @property
+    def height(self):
+        return self.__height
 
-	@height.setter
-	def height(self, value):
-		self.__height = value
+    @height.setter
+    def height(self, value):
+        self.__height = value
 
-	@property
-	def x(self):
-		return self.__x
+    @property
+    def x(self):
+        return self.__x
 
-	@x.setter
-	def x(self, value):
-		self.__x = value
+    @x.setter
+    def x(self, value):
+        self.__x = value
 
-	@property
-	def y(self):
-		return self.__y
+    @property
+    def y(self):
+        return self.__y
 
-	@y.setter
-	def y(self, value):
-		self.__y = value
+    @y.setter
+    def y(self, value):
+        self.__y = value
