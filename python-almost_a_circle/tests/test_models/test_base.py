84df1d3a0141aa09e8340586f9b18244a0ec9452
gasstin <gastonl1994@gmail.com>
2022-09-30 12:31:49 -0300

    fixing task 3

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,11 +1,17 @@
 import unittest
 from models.base import Base
 class Test_Base(unittest.TestCase):
+    
+    def test_change(self):
+        b5 = Base()
+        b5.id = 4
+        self.assertEqual(b5.id, 4)
+    
     def test_creates(self):
         b1 = Base()
-        self.assertEqual(b1.id, 1)
+        self.assertEqual(b1.id, 2)
         b2 = Base(10)
         self.assertEqual(b2.id, 10)
         b3 = Base()
-        self.assertEqual(b3.id, 2)
+        self.assertEqual(b3.id, 3)
         self.assertNotEqual(b2.id, 2)
---
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,25 @@
+import unittest
+from models.rectangle import Rectangle
+class Test_Base(unittest.TestCase):
+    
+    def test_check_position(self):
+        r2 = Rectangle(4, 2, 0, 0, 20)
+        self.assertEqual(r2.x, 0)
+        self.assertNotEqual(r2.y, 1)
+
+    def test_creates(self):
+        r1 = Rectangle(10,2)
+        self.assertEqual(r1.width, 10)
+        self.assertEqual(r1.height, 2)
+        r2 = Rectangle(4, 2, 0, 0, 20)
+        self.assertEqual(r2.id, 20)
+    
+    def test_raise_type_error(self):
+        with self.assertRaises(TypeError):
+            Rectangle(4, 2, "a", 0, 20)
+    
+    def test_raise_value_error(self):
+        with self.assertRaises(ValueError):
+            Rectangle(4, 0, 0, 0, 20)
+        with self.assertRaises(ValueError):
+            Rectangle(4, 3, 0, -2, 20)
