da3ec0ed2d3d9589e84d960ef651601253168830
gasstin <gastonl1994@gmail.com>
2022-10-03 15:11:01 -0300

    fixing task 0

---
+++ python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,56 @@
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+class Test_Base(unittest.TestCase):
+    
+    def test_change(self):
+        b5 = Base()
+        b5.id = 4
+        self.assertEqual(b5.id, 4)
+    
+    def test_creates(self):
+        b1 = Base()
+        self.assertEqual(b1.id, 2)
+        b2 = Base(10)
+        self.assertEqual(b2.id, 10)
+        b3 = Base()
+        self.assertEqual(b3.id, 3)
+        self.assertNotEqual(b2.id, 2)
+    
+    def test_json_s(self):
+        r1 = Rectangle(10, 7, 2, 8)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        self.assertTrue(type(json_dictionary), str)
+        self.assertNotEqual(dictionary, json_dictionary)
+    
+    def test_json_s_static(self):
+        self.assertTrue(Base.__dict__.get("to_json_string"), staticmethod)
+    
+    def test_save_to_file_class(self):
+        self.assertTrue(Base.__dict__.get("to_json_string"), classmethod)
+    
+    def test_save_to_file(self):
+        Rectangle.save_to_file([])
+
+    def test_from_json_string_class(self):
+        self.assertTrue(Base.__dict__.get("from_json_string"), staticmethod)
+
+    def test_from_json_string(self):
+        list_input = [{'id': 89, 'width': 10, 'height': 4},{'id': 7, 'width': 1, 'height': 7}]
+        json_list_input = Rectangle.to_json_string(list_input)
+        list_output = Rectangle.from_json_string(json_list_input)
+    
+    def test_creates(self):
+        r1 = Rectangle(3, 5, 1)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dictionary)
+    
+    def test_load_from_file(self):
+        Rectangle.load_from_file()
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        list_rectangles_input = [r1, r2]
+        Rectangle.save_to_file(list_rectangles_input)
+        list_rectangles_output = Rectangle.load_from_file()
+
---
+++ python-almost_a_circle/tests/test_square.py
@@ -0,0 +1,34 @@
+import unittest
+from models.square import Square
+class Test_Base(unittest.TestCase):
+    
+
+    def test_creates_square(self):
+        s3 = Square(3, 1, 3)
+        print(s3)
+        print(s3.area())
+        s3.display()
+
+    def test_raise_value_error_square(self):
+        with self.assertRaises(ValueError):
+            Square(0, 1, 3)
+    
+    def test_raise_type_error_square(self):
+        s1 = Square(5)
+        with self.assertRaises(TypeError):
+            s1.size = "9"
+            s1.update(1, 2, 3)
+
+    
+    def test_update(self):
+        s1 = Square(5)
+        s1.update(size=7, y=1)
+        print(s1)
+        s1.update(1, 2, 3)
+        print(s1)
+
+    def test_to_dictionary(self):
+        s1 = Square(10, 2, 1)
+        s1_dictionary = s1.to_dictionary()
+        self.assertEqual(s1_dictionary, {'id': 3, 'x': 2, 'size': 10, 'y': 1})
+    
