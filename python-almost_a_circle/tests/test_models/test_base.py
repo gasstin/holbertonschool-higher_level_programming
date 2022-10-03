569dbdcc68e7dc22b9ff77f76869bdbe274c7c09
gasstin <gastonl1994@gmail.com>
2022-10-03 15:07:32 -0300

    adding task 0

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -45,4 +45,12 @@ class Test_Base(unittest.TestCase):
         r1 = Rectangle(3, 5, 1)
         r1_dictionary = r1.to_dictionary()
         r2 = Rectangle.create(**r1_dictionary)
+    
+    def test_load_from_file(self):
+        Rectangle.load_from_file()
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        list_rectangles_input = [r1, r2]
+        Rectangle.save_to_file(list_rectangles_input)
+        list_rectangles_output = Rectangle.load_from_file()
 
