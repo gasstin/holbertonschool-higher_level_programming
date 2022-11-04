dd98ce77dc624ffbe455bfc21c6fae18a4a346cf
gasstin <gastonl1994@gmail.com>
2022-11-04 16:09:03 -0300

    fixing task 14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -12,5 +12,5 @@ ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_shows.title = 'Dexter' -- solo los Dexter
 AND tv_shows.id = tv_show_genres.show_id
 -- comparten el id del tv_show con el id del tv_show_genres
-ORDER BY tv_shows.title ASC;
+ORDER BY tv_genres.name ASC;
                                    
