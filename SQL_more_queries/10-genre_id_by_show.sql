495a82b203344fc015e3e003d01430d0610d405b
gasstin <gastonl1994@gmail.com>
2022-11-04 11:10:16 -0300

    adding task 10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -7,5 +7,5 @@ SELECT tv_shows.title,
     tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres
-WHERE tv_show_genres.genre_id IS NOT NULL
-ORDER BY tv_shows.title ASC;
+WHERE tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
