7f1621fde3949330d35e7afdbcb17287bfdaffa3
gasstin <gastonl1994@gmail.com>
2022-11-04 13:49:19 -0300

    fixing task 11

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -3,9 +3,11 @@
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 -- If a show doesn’t have a genre, display NULL
-SELECT tv_shows.title,
+SELECT
+    tv_shows.title,
     tv_show_genres.genre_id
 FROM tv_shows
-INNER JOIN tv_show_genres
-WHERE tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_show_genres 
+-- Uno los genres a los tv_show y los que no coinciden toman el valor NULL
+ON tv_show_genres.show_id = tv_shows.id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
