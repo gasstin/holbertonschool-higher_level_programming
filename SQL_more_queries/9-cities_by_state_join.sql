d8f189e1ccc6a9b70afd9d5ae3c74a716bf94f60
gasstin <gastonl1994@gmail.com>
2022-11-04 10:34:33 -0300

    fixing task 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,9 @@
 -- task 9
 -- Write a script that lists all cities contained in the database hbtn_0d_usa.
-SELECT cities.id, cities.name, states.name
+SELECT
+    cities.id,
+    cities.name,
+    states.name
 FROM cities
 FULL JOIN states
 ON cities.id = states.state_id
