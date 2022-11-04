26432ffbe4046e4975c5628ee94c55537a63dd1b
gasstin <gastonl1994@gmail.com>
2022-11-04 10:45:16 -0300

    fixing task 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -5,6 +5,6 @@ SELECT
     cities.name,
     states.name
 FROM cities
-FULL JOIN states
-ON cities.id = states.state_id
-ORDER BY cities.id ASC;
+INNER JOIN states
+ON cities.state_id = states.id
+ORDER BY cities.id ASC;                           
