664877a75c6ba2d579ab62772f2264f76f6e3b60
gasstin <gastonl1994@gmail.com>
2022-11-04 14:12:16 -0300

    fixing task 12

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,8 +1,8 @@
 -- task 12
 -- Write a script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
---Each record should display: tv_shows.title - tv_show_genres.genre_id
---Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
---You can use only one SELECT statement
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- You can use only one SELECT statement
 SELECT
     tv_shows.title,
     tv_show_genres.genre_id
