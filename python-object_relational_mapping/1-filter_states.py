53574552d70afaf4ed1f1f0dd314e7aa4f653020
gasstin <gastonl1994@gmail.com>
2022-11-07 11:26:08 -0300

    adding task 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    task 1
+    Write a script that lists all states with a name
+    starting with N (upper N) from the database hbtn_0e_0_usa:
+"""
+
+
+def main():
+    from MySQLdb import connect
+
+    db = connect(host="localhost", port=3306, user=mysql_username,
+                 passwd=mysql_password, db=database_name, charset="utf8")
+    cursor_aux = db.cursor()
+    cursor_aux.execute("SELECT * FROM states WHERE name = 'N' ORDER BY id ASC")
+    rows = cursor_aux.fetchall()
+    for r in rows:
+        print(r)
+    cursor_aux.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    from sys import argv
+
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+    main()
