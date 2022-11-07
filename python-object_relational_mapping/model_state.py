2c2c98d92829f49685a649286ce00d2ee565e5d7
gasstin <gastonl1994@gmail.com>
2022-11-07 16:15:58 -0300

    adding task 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,14 +1,16 @@
 #!/usr/bin/pyhton3
 """
     task 6
-    Write a python file that contains the class definition of a State and an instance Base = declarative_base():    
+    Write a python file that contains the class definition
+    of a State and an instance Base = declarative_base()
 """
 
-from
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
 
 class State(Base):
     """creates a state
@@ -17,5 +19,6 @@ class State(Base):
         Base (_type_): _description_
     """
     __tablename__ = 'states'
-    id = Column(Integer, nullable=False, unique=True, autoincrement=True, primary_key= True)
-    name = Column(String(128), nulleable=False)
+    id = Column(Integer, nullable=False, unique=True,
+                autoincrement=True, primary_key=True)
+    name = Column(String(128), nullable=False)
