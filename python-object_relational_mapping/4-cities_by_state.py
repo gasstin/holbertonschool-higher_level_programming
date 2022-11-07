b1d85f7b6c54ae9ee04977fd0a8edb8a4bd76215
gasstin <gastonl1994@gmail.com>
2022-11-07 14:01:46 -0300

    adding task 4

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    task 4
+    Write a script that lists all cities from the database hbtn_0e_4_usa
+"""
+
+
+def main():
+    from MySQLdb import connect
+
+    db = connect(host="localhost", port=3306, user=mysql_username,
+                 passwd=mysql_password, db=database_name, charset="utf8")
+    cursor_aux = db.cursor()
+    cursor_aux.execute("SELECT * FROM cities INNER JOIN states\
+        ON state_id.cities=id.cities ORDER BY cities.id ASC")
+    rows = cursor_aux.fetchall()
+    for r in rows:
+        print(r)
+    cursor_aux.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    from sys import argv
+
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+    main()
