93d6209c0f442741ad40c6bdc50bbfcc6a4480d1
gasstin <gastonl1994@gmail.com>
2022-11-07 14:37:08 -0300

    fixing task 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
     task 5
-    Write a script that takes in the name of a state as an argument
+    Write script that takes in the name of a state as an argument
     and lists all cities of that state,
     using the database hbtn_0e_4_usa
 """
