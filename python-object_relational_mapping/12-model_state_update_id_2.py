7f0a7d40add424b4b6674680ab76e66a372e0f97
gasstin <gastonl1994@gmail.com>
2022-11-08 13:12:47 -0300

    fixing task 12 - pycodestyle

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
     task 12
-    Write a script that changes the name of a State object 
+    Write a script that changes the name of a State object
     from the database hbtn_0e_6_usa
 """
 
