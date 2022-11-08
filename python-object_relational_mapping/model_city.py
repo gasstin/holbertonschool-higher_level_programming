c8ff4cb9078cd83a92522522178180d8d1e2653d
gasstin <gastonl1994@gmail.com>
2022-11-08 14:48:43 -0300

    fixing task 14 - fixing models

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -6,7 +6,7 @@
 """
 
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
 
 class City(Base):
@@ -19,5 +19,5 @@ class City(Base):
     id = Column(Integer, nullable=False, unique=True,
                 autoincrement=True, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, nullable=False, ForeingKey('states.id'))
+    state_id = Column(Integer, ForeignKey('states.id'))
     
