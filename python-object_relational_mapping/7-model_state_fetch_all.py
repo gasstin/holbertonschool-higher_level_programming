839abfbb53f11e4560d65a253160783e725b754b
gasstin <gastonl1994@gmail.com>
2022-11-08 09:54:22 -0300

    adding task 7

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    task 6
+    Write a python file that contains the class definition
+    of a State and an instance Base = declarative_base().
+"""
+
+from sqlalchemy import create_engine, Session
+from model_state import Base, State
+from sys import argv
+
+if __name__ == "__main__":
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+    
+    engine = create_engine(f'mysql+mysqldb://\
+            {mysql_username}:{mysql_password}@localhost:3306/{database_name}', pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    
+    session = Session(engine)
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+    session.close()
+
+
+
+
