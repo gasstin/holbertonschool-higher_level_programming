9e5574a65a35ce3c38f719e61ffc097203e48cb3
gasstin <gastonl1994@gmail.com>
2022-11-08 15:08:43 -0300

    fixing task 14 - fixing filter

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
     task 14
-    write a script 14-model_city_fetch_by_state.py that prints 
+    write a script 14-model_city_fetch_by_state.py that prints
     all City objects from the database hbtn_0e_14_usa:
 """
 
@@ -26,5 +26,5 @@ if __name__ == "__main__":
     for city in session.query(City).order_by(City.id).all():
         for state in session.query(State).order_by(State.id).all():
             if state.id == city.state_id:
-                print(f"{state.name}: {city.id} {city.name}")
+                print(f"{state.name}: ({city.id}) {city.name}")
     session.close()
