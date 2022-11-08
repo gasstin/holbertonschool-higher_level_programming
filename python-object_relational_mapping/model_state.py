35ee5b118c1d3c56144aa3cb4fa5e894ad0b204c
gasstin <gastonl1994@gmail.com>
2022-11-08 15:18:14 -0300

    fixing task 13

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -8,7 +8,6 @@
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -23,4 +22,3 @@ class State(Base):
     id = Column(Integer, nullable=False, unique=True,
                 autoincrement=True, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City")
