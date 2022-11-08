585c6a0e9b6b5935f7ef25e8f677170e4899d93f
gasstin <gastonl1994@gmail.com>
2022-11-08 11:43:08 -0300

    adding task 10

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+    task 10
+    Write a script that prints the State object
+    with the name passed as argument from the database hbtn_0e_6_usa
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from sys import argv
+
+if __name__ == "__main__":
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+    state_name_searched = argv[4]
+
+    engine = create_engine(f'mysql+mysqldb://\
+{mysql_username}:{mysql_password}@localhost:3306/{database_name}',
+                           pool_pre_ping=True,)
+    Base.metadata.create_all(engine)  # Create an engine
+
+    session = Session(engine)  # Create a session
+
+    find = 0
+    for state in session.query(State).order_by(State.id)\
+            .filter_by(name=state_name_searched):
+        print(f"{state.id}")
+        find = 1
+    if find:
+        print("Not found")
+    session.close()
