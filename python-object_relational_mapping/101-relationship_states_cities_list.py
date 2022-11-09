19368295b0ac24876091c2ff8f1dab4def274d30
gasstin <gastonl1994@gmail.com>
2022-11-09 10:17:29 -0300

    adding task 16

---
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+    task 16
+    Write a script that lists all State objects,
+    and corresponding City objects, contained
+    in the database hbtn_0e_101_usa
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from relationship_state import Base, State
+from relationship_city import City
+from sys import argv
+
+if __name__ == "__main__":
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+
+    engine = create_engine(f'mysql+mysqldb://\
+{mysql_username}:{mysql_password}@localhost:3306/{database_name}',
+                           pool_pre_ping=True,)
+    Base.metadata.create_all(engine)  # Create an engine
+
+    session = Session(engine)  # Create a session
+
+    for state in session.query(State).order_by(State.id).all():
+        print(f"{state.id}: {state.name}")
+        for city in session.query(City).order_by(City.id).all():
+            if state.id == city.state_id:
+                print(f"\t{city.id}: {city.name}")
+    session.close()
