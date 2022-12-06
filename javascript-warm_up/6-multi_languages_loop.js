fbadc8c2482de501bca0b6e7b3f52b414e186937
gasstin <gastonl1994@gmail.com>
2022-12-06 13:56:11 +0000

    adding task 6

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* task 6
+    Write a script that prints 3 lines but by using an array of string and a loop
+*/
+
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+lines.forEach(element => {
+  console.log(element);
+});
