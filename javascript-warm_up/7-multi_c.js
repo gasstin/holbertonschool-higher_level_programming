6ba4e8c51040602090276bcb07e0d0ebd38483f4
gasstin <gastonl1994@gmail.com>
2022-12-06 14:53:24 +0000

    fixing task 7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,10 +2,10 @@
 /* task 7
     Write a script that prints x times “C is fun”
 */
-import { argv } from 'node:process';
+require('process');
 
-if (parseInt(argv[2])) {
-  for (let index = 0; index < parseInt(argv[2]); index++) {
+if (parseInt(process.argv[2])) {
+  for (let index = 0; index < parseInt(process.argv[2]); index++) {
     console.log('C is fun');
   }
 } else {
