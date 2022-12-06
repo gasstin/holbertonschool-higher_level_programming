f04d4ce06a21d545555a7f64735070d28e3610fe
gasstin <gastonl1994@gmail.com>
2022-12-06 13:29:20 +0000

    adding task 2

---
+++ javascript-warm_up/2-arguments.mjs
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* task 2 */
+import argv from 'node:process';
+
+if (argv[3]) {
+  console.log('Arguments found');
+} else if (argv[2]) {
+  console.log('Argument found');
+} else {
+  console.log('No argument');
+}
