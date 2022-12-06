b1302b9a969cf1c8ae94dc589f870decd88e6228
gasstin <gastonl1994@gmail.com>
2022-12-06 13:33:57 +0000

    fixing task 2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 /* task 2 */
 import { argv } from 'node:process';
 
-if (argv[2]) {
+if (argv[3]) {
   console.log('Arguments found');
-} else if (argv[1]) {
+} else if (argv[2]) {
   console.log('Argument found');
 } else {
   console.log('No argument');
