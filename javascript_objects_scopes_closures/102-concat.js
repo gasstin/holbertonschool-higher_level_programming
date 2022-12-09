b4ff9b5d5a377bee3dda0b49b8b1885345f5c943
gasstin <gastonl1994@gmail.com>
2022-12-09 15:11:58 +0000

    fxing task 13

--- javascript_objects_scopes_closures/102-concat.js
+++ javascript_objects_scopes_closures/102-concat.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) throw err;
-  fs.writeFile(process.argv[4], data + '\n', (err1) => {
+  fs.writeFile(process.argv[4], data, (err1) => {
     // In case of a error throw err
     if (err1) throw err;
   });
