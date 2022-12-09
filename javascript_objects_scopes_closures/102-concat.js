63c5067f2f2bedce30419d3c774be47588a12ecd
gasstin <gastonl1994@gmail.com>
2022-12-09 15:03:00 +0000

    adding task 13

---
+++ javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+/* task 13
+    Write a script that imports a dictionary of occurrences by user id
+    and computes a dictionary of user ids by occurrence.
+*/
+require('process');
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) throw err;
+  fs.writeFile(process.argv[4], data + '\n', (err1) => {
+    // In case of a error throw err
+    if (err1) throw err;
+  });
+});
+fs.readFile(process.argv[3], 'utf8', function (err, data) {
+  if (err) throw err;
+  fs.appendFile(process.argv[4], data, (err1) => {
+    // In case of a error throw err
+    if (err1) throw err;
+  });
+});
