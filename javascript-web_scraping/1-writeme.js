d8ba323b99d553269a4c54bc07224613865175c3
gasstin <gastonl1994@gmail.com>
2022-12-12 13:17:36 +0000

    adding task 1

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* task 1
+Write a script that writes a string to a file.
+*/
+require('process');
+const fs = require('fs');
+
+fs.writeFile(process.argv[2], process.argv[3], (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
