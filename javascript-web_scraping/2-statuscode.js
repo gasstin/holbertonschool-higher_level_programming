76799533cb43fde0e076f86cb6a3d0311f7093d8
gasstin <gastonl1994@gmail.com>
2022-12-12 13:28:02 +0000

    adding task 2

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/* task 2
+Write a script that display the status code of a GET request.
+*/
+require('process');
+const request = require('request');
+
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error); // print error if occurs
+  }
+  console.log('code: ', response.statusCode);
+});
