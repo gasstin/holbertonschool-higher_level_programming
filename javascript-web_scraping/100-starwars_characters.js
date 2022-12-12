1488b412eb5486f0efd8345c4650464d499df5ba
gasstin <gastonl1994@gmail.com>
2022-12-12 19:15:20 +0000

    adding task 7

---
+++ javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+/* task 7
+Write a script that prints all characters of a Star Wars movie:
+*/
+require('process');
+const request = require('request');
+
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error); // print error if occurs
+  } else {
+    JSON.parse(body).characters.forEach(element => {
+      request(element, function(error, response, body) {
+        console.log(JSON.parse(body).name);
+      }
+      )
+    });
+  }
+});
