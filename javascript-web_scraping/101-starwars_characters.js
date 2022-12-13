7b86d5d5809ace235dab5332c7f4a96085dedd78
gasstin <gastonl1994@gmail.com>
2022-12-13 17:18:14 +0000

    fixing task 8

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -5,20 +5,33 @@ Write a script that prints all characters of a Star Wars movie in right order:
 require('process');
 const request = require('request');
 
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2] + '/';
+///////////////////
 
-for (let index = 1; index <= 9; index++) {
-  const urlToRequest = 'https://swapi-api.alx-tools.com/api/people/?page=' + index;
-  request(urlToRequest, function (error, response, body) {
+function request_actors (url, index) {
+  const urlToCheck = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2] + '/';
+  let actorsList = '';
+  request(url, function (error, response, body) {
     if (error) {
       console.error(error); // print error if occurs
     } else {
       const actors = JSON.parse(body).results;
       actors.forEach(element => {
-        if (element.films.includes(url)) {
-          console.log(element.name);
+        if (element.films.includes(urlToCheck)) {
+          actorsList += index + ',' + element.name + ',';
         }
       });
-    }
-  });
+      } 
+      actorsList = actorsList.split(',');
+      for (let indexAux = 0; indexAux < actorsList.length; indexAux++) {
+          if (actorsList[indexAux] == index) {
+            console.log(actorsList[indexAux + 1]);
+          }
+      }
+      })
+  };
+
+for (let index = 1; index <= 9; index++) {
+const urlToRequest = 'https://swapi-api.alx-tools.com/api/people/?page=' + index;
+  request_actors(urlToRequest, index);
 }
+  
