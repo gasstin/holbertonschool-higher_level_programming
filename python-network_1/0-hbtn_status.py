62e77a6b8153594437e7ac7c9dc52a67252bc978
gasstin <gastonl1994@gmail.com>
2023-01-17 14:36:44 -0300

    fixing content

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -8,7 +8,9 @@ if __name__ == "__main__":
     from urllib import request
     url = "https://intranet.hbtn.io/status"
     with request.urlopen(url) as response:
-        print("Body response:")
-        print(f"\t- type: {type(response.read())}")
-        print(f"\t- content: {response.read()}")
-        print(f"\t- utf8 content: {response.msg}")
+        body = response
+        text_body = body.read()
+    print("Body response:")
+    print(f"\t- type: {type(body.read())}")
+    print(f"\t- content: {text_body}")
+    print(f"\t- utf8 content: {body.msg}")
