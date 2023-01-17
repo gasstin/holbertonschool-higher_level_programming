e224d78d146e1eb22b11aef64d12ef92313368d3
gasstin <gastonl1994@gmail.com>
2023-01-17 11:02:47 -0300

    fixing space at the begin

--- python-network_0/3-methods.sh
+++ python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -s -i -L -X OPTIONS "$1" | grep -E 'Allow' | cut -d ':' -f 2
+curl -s -i -L -X OPTIONS "$1" | grep -E 'Allow' | cut -d ' ' -f 2-
