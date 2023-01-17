da3373e3549132b271538e7982f36cc2150b7638
gasstin <gastonl1994@gmail.com>
2023-01-17 16:07:35 -0300

    adding task 2

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    Python script that takes in a URL and an email,
+    sends a POST request to the passed URL
+    with the email as a parameter,
+    and displays the body of the response (decoded in utf-8).
+"""
+
+
+if __name__ == "__main__":
+    from urllib import request, parse
+    from sys import argv
+
+    url = argv[1]
+    new_data = {'email': argv[2]}
+
+    data = parse.urlencode(new_data).encode('utf8')
+    new_request = request.Request(url, data)
+    with request.urlopen(new_request) as response:
+        if response.read().decode('utf8') == 'OK':
+            print(f"Your email is: {argv[2]}")
