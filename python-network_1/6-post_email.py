b630d82f4e23ff1a1326aa768635c954cf45d5f3
gasstin <gastonl1994@gmail.com>
2023-01-17 17:53:00 -0300

    adding task 6

---
+++ python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    Python script that takes in a URL, sends a request to the URL
+    and displays the value of the X-Request-Id variable found
+    in the header of the response.
+
+    You must use the packages requests and sys
+"""
+
+
+if __name__ == "__main__":
+    from requests import post, get
+    from sys import argv
+
+    req = post(argv[1], data={'email': argv[2]})
+    print(f"Your email is: {argv[2]}")
