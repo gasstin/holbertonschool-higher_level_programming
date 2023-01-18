50dbf59fd34cdff6860c6909bef8ad1bfe0f2deb
gasstin <gastonl1994@gmail.com>
2023-01-18 18:00:48 +0000

    adding task 8

---
+++ python-network_1/8-json_api.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    Write a Python script that takes in a letter and sends a POST request
+    to http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+
+if __name__ == "__main__":
+    from requests import get, post
+    from sys import argv
+
+    req = post('http://0.0.0.0:5000/search_user', data={'q': argv[1]})
+    req = get('http://0.0.0.0:5000/search_user')
+    req_json = req.json()
+    if not req_json:
+        print('No result')
+    else:
+        print(req_json)
