a0956553c655d00cc877a8d2ad20881c3eb00392
gasstin <gastonl1994@gmail.com>
2023-01-18 18:22:19 +0000

    fixing no result

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -6,15 +6,20 @@
 
 
 if __name__ == "__main__":
-    from requests import get, post
+    from requests import get, post, exceptions
     from sys import argv
 
-    req = post('http://0.0.0.0:5000/search_user', data={'q': argv[1]})
+    if len(argv) == 1:
+        data_to_post = None
+    else:
+        data_to_post = argv[1]
+
+    req = post('http://0.0.0.0:5000/search_user', data={'q': data_to_post})
     try:
         req_json = req.json()
         if not req_json:
             print('No result')
         else:
             print(f"[{req_json['id']}] {req_json['name']}")
-    except:
+    except exceptions.JSONDecodeError:
         print('Not a valid JSON')
